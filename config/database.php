38165e8dc8d67a3b2a14e4624fe737f4dc84f2ce
Vikzdimension <vivek.lode1@gmail.com>
2025-04-25 13:33:45 +0530

    DB connection set and use without DB name
    DB connection set and use without DB name

--- config/database.php
+++ config/database.php
@@ -6,10 +6,16 @@ $user = 'root';
 $pass = 'root@123';
 $charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
 
 try{
-    $pdo = new PDO($dsn, $user, $pass);
+    $pdo = new PDO($dsn, $user, $pass, $options);
 }catch(PDOException $e){
     throw new PDOException($e->getMessage(), (int)$e->getCode());
 }
