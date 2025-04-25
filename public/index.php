ccda9a1c78fe981254970dbce68906cdcf20590d
Vikzdimension <vivek.lode1@gmail.com>
2025-04-25 16:30:35 +0530

    style.css added and public index html removed
    style.css added and public index html removed

--- public/index.php
+++ public/index.php
@@ -37,33 +37,3 @@ try {
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="WPOETS - A simple PHP MVC framework">
-    <meta name="keywords" content="PHP, MVC, Framework, WPOETS">
-    <meta name="author" content="Vivek Lode">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-    <title>
-        <?= $title ?? "WPOETS"; ?>
-    </title>
-</head>
-
-<body class="bg-light">
-    <div class="container">
-        <h1 class="text-primary"> <?= $message ?? 'Welcome!' ?></h1>
-        <p> This is the view rendered using MVC</p>
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
